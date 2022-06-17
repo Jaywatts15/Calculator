8214cff6ab6a75426a9ab5576e7415b1e7ba71b4
Jaywatts15 <74090059+Jaywatts15@users.noreply.github.com>
2022-06-16 21:53:45 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// This function clear all the values
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+
+// This function display values
+function display(value) {
+  document.getElementById('result').value += value;
+  console.log();
+}
+// This function evaluates the expression and return result
+function calculate() {
+  let p = document.getElementById("result").value;
+  let q = eval(p);
+  document.getElementById("result").value = q;
+}
